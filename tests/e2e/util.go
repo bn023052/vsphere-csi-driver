ac9aa5568e24985e1d67f1883165802e70780108
bn023052 <bhavya.nayak@broadcom.com>
2025-06-20 15:36:49 +0530

    Added a testcase for cluster-distribution

--- tests/e2e/util.go
+++ tests/e2e/util.go
@@ -7876,3 +7876,11 @@ func staticProvisioningPreSetUpUtil(ctx context.Context, f *framework.Framework,
 
 	return restConfig, storageclass, profileID
 }
+
+// Generates random string of requested length
+func randomString(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	b := make([]byte, length+2)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)[2 : length+2]
+}
